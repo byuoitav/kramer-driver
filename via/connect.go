a230185a5b19ac864c910a5ac8027b4a00c93125
Clinton Reeder <clinton_reeder@byu.edu>
2020-03-05 17:22:49 -0700

    Fixing Bufio.Reader issues

--- via/connect.go
+++ via/connect.go
@@ -5,6 +5,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"net"
+	"regexp"
 
 	"time"
 
@@ -36,8 +37,12 @@ func (v *VIA) SendCommand(command Command, addr string) (string, error) {
 	conn.SetReadDeadline(time.Now().Add(timeoutDuration))
 
 	// login
-	login(conn, Username, Password)
-
+	//login(conn, Username, Password)
+	err = login(conn, Username, Password)
+	if err != nil {
+		log.L.Debugf("Houston, we have a problem logging in. The login failed")
+		return "", err
+	}
 	// write command
 	if len(command.Command) > 0 {
 		command.addAuth(Username, Password, false)
@@ -64,21 +69,7 @@ func login(conn *net.TCPConn, ViaUser, ViaPass string) error {
 	cmd.addAuth(ViaUser, ViaPass, true)
 	cmd.Command = "Login"
 
-	log.L.Infof("Logging in...")
-	log.L.Infof("Username: %s", ViaUser)
-	err := cmd.writeCommand(conn)
-	if err != nil {
-		return err
-	}
-
-	log.L.Infof("Login successful")
-
-	return nil
-}
-
-func (c *Command) writeCommand(conn *net.TCPConn) error {
-
-	// read welcome message
+	// read welcome message (Only Important when we first open a connection and login)
 	reader := bufio.NewReader(conn)
 	_, err := reader.ReadBytes('\n')
 	if err != nil {
@@ -86,6 +77,44 @@ func (c *Command) writeCommand(conn *net.TCPConn) error {
 		log.L.Error(err.Error())
 		return err
 	}
+
+	log.L.Infof("Logging in...")
+	log.L.Debugf("Username: %s", ViaUser)
+	err = cmd.writeCommand(conn)
+	if err != nil {
+		return err
+	}
+	//reader := bufio.NewReader(conn)
+	resp, err := reader.ReadBytes('\n')
+	if err != nil {
+		err = fmt.Errorf("error reading from system: %s", err.Error())
+		log.L.Error(err.Error())
+		return err
+	}
+
+	s := string(resp)
+
+	errRx := regexp.MustCompile(`Error`)
+	SuccessRx := regexp.MustCompile(`Successful`)
+	respRx := errRx.MatchString(s)
+	SuccessResp := SuccessRx.MatchString(s)
+
+	if respRx == true {
+		log.L.Infof("Response from device: %s", s)
+		err := fmt.Errorf("Unable to login due to an error: %s", s)
+		return err
+	}
+
+	if SuccessResp == true {
+		log.L.Debugf("Connection is successful, We are connected: %s", s)
+	}
+
+	//log.L.Infof("Login successful")
+
+	return nil
+}
+
+func (c *Command) writeCommand(conn *net.TCPConn) error {
 
 	b, err := xml.Marshal(c)
 	if err != nil {
@@ -129,7 +158,7 @@ func getConnection(address string) (*net.TCPConn, error) {
 
 //Create a persistent connection in order to catch actions and events that are printed
 //out on console. This includes login, logoff, media presentation, and sharing events
-func PersistConnection(addr string) (*net.TCPConn, error) {
+func PersistConnection(addr, ViaUser, ViaPass string) (*net.TCPConn, error) {
 	// get the connection
 	log.L.Infof("Opening persistent telnet connection for reading events from %s", addr)
 	pconn, err := getConnection(addr)
@@ -138,7 +167,11 @@ func PersistConnection(addr string) (*net.TCPConn, error) {
 	}
 
 	// login
-	login(pconn, ViaUser, ViaPass)
+	err = login(pconn, ViaUser, ViaPass)
+	if err != nil {
+		log.L.Debugf("Houston, we have a problem logging in. The login failed")
+		return nil, err
+	}
 
 	return pconn, nil
 }
